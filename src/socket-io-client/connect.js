ac03de0966682600274f9b3fb6c9fa133c34ead3
Csaba Kigyosi <csabronc@gmail.com>
2018-11-24 20:37:10 +0100

    change url of connect

--- src/socket-io-client/connect.js
+++ src/socket-io-client/connect.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000', { forceNew: false });
-//const socket = io('https://safe-crag-14775.herokuapp.com', { forceNew: false });
+//const socket = io('http://localhost:5000', { forceNew: false });
+const socket = io('https://safe-crag-14775.herokuapp.com', { forceNew: false });
 
 export default socket;
